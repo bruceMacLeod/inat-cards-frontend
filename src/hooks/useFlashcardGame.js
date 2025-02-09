c5a1af969a44bf21fa425c4a9468cde970d8f88f
Bruce MacLeod <macleod@maine.edu>
2025-02-08 19:35:59 -0500

    checkanswer trim string

--- src/hooks/useFlashcardGame.js
+++ src/hooks/useFlashcardGame.js
@@ -17,8 +17,8 @@ export const useFlashcardGame = (currentCard) => {
         }
 
         const userAnswer = hint !== null ? String(hint) : String(answer);
-        console.log(userAnswer.toLowerCase(), currentCard.scientific_name.toLowerCase());
-        const isCorrect = userAnswer.toLowerCase() === currentCard.scientific_name.toLowerCase();
+//        console.log(userAnswer.toLowerCase(), currentCard.scientific_name.toLowerCase());
+        const isCorrect = userAnswer.trim().toLowerCase() === currentCard.scientific_name.trim().toLowerCase();
         const taxaUrl = currentCard.taxa_url;
         const hyperlinkedName = `<a href="${taxaUrl}" target="_blank" rel="noopener noreferrer">${currentCard.scientific_name}</a>`;
         const hyperlinkedCommonName = currentCard.common_name
